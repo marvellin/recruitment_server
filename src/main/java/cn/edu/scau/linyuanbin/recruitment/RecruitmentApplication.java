6d7bc1a6b35b9919d014c48091493976c4c7f629
marvellin <1021478667@qq.com>
2020-03-23 17:27:51 +0800

    0323 初步完成所有mapper.xml，统一后台返回数据格式

--- src/main/java/cn/edu/scau/linyuanbin/recruitment/RecruitmentApplication.java
+++ src/main/java/cn/edu/scau/linyuanbin/recruitment/RecruitmentApplication.java
@@ -3,6 +3,7 @@ package cn.edu.scau.linyuanbin.recruitment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RecruitmentApplication {
 
