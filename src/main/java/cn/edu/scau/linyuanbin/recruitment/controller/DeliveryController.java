d0940ea1966ddeb2cd30f90953b7d7561b0ac46c
marvellin <1021478667@qq.com>
2020-03-26 16:47:01 +0800

    0326 controller层相关依赖完善

--- src/main/java/cn/edu/scau/linyuanbin/recruitment/controller/DeliveryController.java
+++ src/main/java/cn/edu/scau/linyuanbin/recruitment/controller/DeliveryController.java
@@ -1,9 +1,18 @@
 package cn.edu.scau.linyuanbin.recruitment.controller;
 
+import cn.edu.scau.linyuanbin.recruitment.service.service.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author: linyuanbin
  * @Description:
  * @Date: Created in 19:29 2020/3/25
  */
+@RestController
+@RequestMapping("/delivery")
 public class DeliveryController {
+    @Autowired
+    DeliveryService service;
 }
--- src/main/java/cn/edu/scau/linyuanbin/recruitment/controller/ProjectExperienceController.java
+++ src/main/java/cn/edu/scau/linyuanbin/recruitment/controller/ProjectExperienceController.java
@@ -1,9 +1,18 @@
 package cn.edu.scau.linyuanbin.recruitment.controller;
 
+import cn.edu.scau.linyuanbin.recruitment.service.service.ProjectExperienceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author: linyuanbin
  * @Description:
  * @Date: Created in 19:29 2020/3/25
  */
+@RestController
+@RequestMapping("/projectExperience")
 public class ProjectExperienceController {
+    @Autowired
+    ProjectExperienceService service;
 }
